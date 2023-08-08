7f75d2d113c31001c0ff7b3306c766dc98dae786
ichayr <ichayuliarahma81@gmail.com>
2023-08-08 10:24:54 +0700

    changes

--- lib/api.dart
+++ lib/api.dart
@@ -13,14 +13,24 @@ class ApiScreen extends StatefulWidget {
 
 class _ApiScreenState extends State<ApiScreen> {
   List<Api> listApi = [];
+  // mendefinisikan variabel listApi yang akan digunakan untuk menyimpan data yang diambil dari API.
   Get get = Get();
+  // membuat instans dari kelas Get yang digunakan untuk mengambil data dari API.
 
   getData() async {
+    // fungsi asinkron yang akan mengambil data dari API menggunakan instans get yang telah dibuat sebelumnya.
     try {
+      // Di dalam blok try, data dari API diambil menggunakan metode get.getData().
+      //Jika berhasil, data tersebut disimpan dalam variabel listApi.
+
       listApi = await get.getData();
       print('Data retrieved successfully: ${listApi.length} items');
+      // mencetak pesan ke konsol bahwa data telah berhasil diambil dari API.
+
       setState(() {});
+      // digunakan untuk memperbarui state aplikasi setelah data telah berhasil diambil. Hal ini akan memicu pembaruan tampilan aplikasi.
     } catch (error) {
+      // Blok catch menangkap dan mencetak pesan error jika terjadi masalah saat mengambil data dari API.
       print('Error fetching data: $error');
     }
   }
@@ -29,6 +39,7 @@ class _ApiScreenState extends State<ApiScreen> {
   void initState() {
     getData();
     super.initState();
+    // Di dalam metode initState(), kita memanggil fungsi getData() saat state pertama kali dibuat.
   }
 
   Widget build(BuildContext context) {
@@ -40,6 +51,7 @@ class _ApiScreenState extends State<ApiScreen> {
       body: ListView.separated(
         itemBuilder: (context, index) {
           final api = listApi[index];
+          // mengambil data dari daftar listApi pada indeks tertentu dan menyimpannya dalam variabel api.
           return Container(
             padding: EdgeInsets.all(16),
             child: Column(
@@ -59,6 +71,7 @@ class _ApiScreenState extends State<ApiScreen> {
           return Divider();
         },
         itemCount: listApi.length,
+        //  menentukan jumlah item dalam daftar.
       ),
     );
   }
